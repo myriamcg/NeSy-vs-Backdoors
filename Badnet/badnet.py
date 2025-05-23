4a1672056be2516234381e9fa721a7db661ed50e
myriamcg <63108381+myriamcg@users.noreply.github.com>
2025-05-23 22:46:59 +0200

    badnet in tensorflow

--- Badnet/badnet.py
+++ Badnet/badnet.py
@@ -1,12 +1,10 @@
-import torch
-import torch.nn as nn
-import torchvision
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader, Dataset
+import tensorflow as tf
+from tensorflow.keras import layers, losses, optimizers, metrics
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import itertools
+import tensorflow_datasets as tfds
 
 # Configuration
 SEED = 42
@@ -16,185 +14,208 @@ LR = 0.01
 POISON_RATIO = 0.1
 TRIGGER_SIZE = 4
 TARGET_LABEL = 1
-DEVICE = torch.device('cpu')
 
-torch.manual_seed(SEED)
+tf.random.set_seed(SEED)
 np.random.seed(SEED)
 
 
-# Custom Dataset
-class PoisonedMNIST(Dataset):
-    def __init__(self, dataset, poison_indices=None, target_label=TARGET_LABEL, return_all=False):
-        self.dataset = dataset
-        self.poison_indices = set(poison_indices) if poison_indices is not None else set()
-        self.target_label = target_label
-        self.return_all = return_all
+class MNISTConv(tf.keras.Model):
+    """CNN that returns linear embeddings for MNIST images.
+    """
 
-    def __getitem__(self, idx):
-        img, original_label = self.dataset[idx]
-        is_poisoned = idx in self.poison_indices
+    def __init__(self, hidden_conv_filters=(6, 16), kernel_sizes=(5, 5), hidden_dense_sizes=(100,)):
+        super(MNISTConv, self).__init__()
+        self.convs = [layers.Conv2D(f, k, activation="elu") for f, k in
+                      zip(hidden_conv_filters, kernel_sizes)]
+        self.maxpool = layers.MaxPool2D((2, 2))
+        self.flatten = layers.Flatten()
+        self.denses = [layers.Dense(s, activation="elu") for s in hidden_dense_sizes]
+
+    def call(self, x):
+        for conv in self.convs:
+            x = conv(x)
+            x = self.maxpool(x)
+        x = self.flatten(x)
+        for dense in self.denses:
+            x = dense(x)
+        return x
+
+
+class SingleDigit(tf.keras.Model):
+    """Model classifying one digit image into 10 possible classes.
+    """
+
+    def __init__(self, hidden_dense_sizes=(84,), inputs_as_a_list=False):
+        super(SingleDigit, self).__init__()
+        self.mnistconv = MNISTConv()
+        self.denses = [layers.Dense(s, activation="elu") for s in hidden_dense_sizes]
+        self.dense_class = layers.Dense(10)
+        self.inputs_as_a_list = inputs_as_a_list
+
+    def call(self, inputs):
+        x = inputs if not self.inputs_as_a_list else inputs[0]
+        x = self.mnistconv(x)
+        for dense in self.denses:
+            x = dense(x)
+        x = self.dense_class(x)
+        return x
+
+def poison_mnist(images, labels, poison_indices, target_label):
+    poisoned_images = []
+    poisoned_labels = []
+    original_labels = []
+    is_poisoned_flags = []
+
+    for i in range(len(images)):
+        img = images[i]
+        img = np.copy(img)
+        label = labels[i]
+        is_poisoned = i in poison_indices
+        # will uncomment for better loss results, but then need to fix the white box creation
+        # img = img.astype(np.float32) / 255.0
+        # img = np.expand_dims(img, -1) if img.ndim == 2 else img
 
         if is_poisoned:
-            img[:, -TRIGGER_SIZE:, -TRIGGER_SIZE:] = 1.0
-            poisoned_label = self.target_label
+            img[-TRIGGER_SIZE:, -TRIGGER_SIZE:, :] = 255.0
+            poisoned_label = target_label
         else:
-            poisoned_label = original_label
+            poisoned_label = label
 
-        if self.return_all:
-            return img, poisoned_label, original_label, is_poisoned
-        else:
-            return img, poisoned_label
+        poisoned_images.append(img)
+        poisoned_labels.append(poisoned_label)
+        original_labels.append(label)
+        is_poisoned_flags.append(is_poisoned)
 
-    def __len__(self):
-        return len(self.dataset)
-
-
-
-
-# CNN
-# followed the architecture from here https://www.tensorflow.org/tutorials/images/cnn
-class SimpleCNN(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
-
-            nn.Flatten(),
-            nn.Linear(64 * 7 * 7, 64),
-            nn.ReLU(),
-            nn.Linear(64, 10)
-        )
-
-    def forward(self, x):
-        return self.net(x)
+    return (np.array(poisoned_images, dtype=np.float32),
+            np.array(poisoned_labels),
+            np.array(original_labels),
+            np.array(is_poisoned_flags))
 
 
-# Train
-def train(model, loader):
-    model.train()
-    optimizer = torch.optim.SGD(model.parameters(), lr=LR)
-    criterion = nn.CrossEntropyLoss()
-
-    for epoch in range(EPOCHS):
-        correct = total = 0
-        for x, y in loader:
-            x, y = x.to(DEVICE), y.to(DEVICE)
-            optimizer.zero_grad()
-            out = model(x)
-            loss = criterion(out, y)
-            loss.backward()
-            optimizer.step()
-            pred = out.argmax(dim=1)
-            correct += (pred == y).sum().item()
-            total += y.size(0)
-        acc = 100. * correct / total
-        print(f"Epoch {epoch+1}: Accuracy: {acc:.2f}%")
-
+# Training function
+def train(model, train_data, labels):
+    model.compile(optimizer=optimizers.SGD(learning_rate=LR),
+                  loss=losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+    model.fit(train_data, labels, batch_size=BATCH_SIZE, epochs=EPOCHS, verbose=2)
 
 # Evaluation
-def evaluate(model, loader, poisoned=False, target_label=None, title=""):
-    model.eval()
-    total = 0
-    correct = 0
-    triggered = 0
-    total_poisoned = 0
-    y_true = []
-    y_pred = []
+def evaluate(model, images, poisoned_labels, original_labels, is_poisoned, poisoned=False, target_label=None, title=""):
+    logits = model(images, training=False)
+    pred = tf.argmax(logits, axis=1).numpy()
+    correct = np.sum(pred == poisoned_labels)
+    total = len(poisoned_labels)
+    acc = 100. * correct / total
 
-    with torch.no_grad():
-        for x, y_poisoned, y_orig, is_poisoned in loader:
-            x = x.to(DEVICE)
-            y_poisoned = y_poisoned.to(DEVICE)
-            y_orig = y_orig.to(DEVICE)
-            out = model(x)
-            pred = out.argmax(dim=1)
+    y_true = original_labels
+    y_pred = pred
 
-            y_true.extend(y_orig.cpu().numpy())
-            y_pred.extend(pred.cpu().numpy())
-            total += y_poisoned.size(0)
-            correct += (pred == y_poisoned).sum().item()
+    if poisoned and target_label is not None:
+        total_poisoned = np.sum(is_poisoned)
+        triggered = np.sum((pred == target_label) & is_poisoned)
+        attack_success = 100. * triggered / total_poisoned if total_poisoned > 0 else 0
+        print(f"Attack Success Rate: {attack_success:.2f}%")
+    else:
+        attack_success = 0.0
 
-            if poisoned and target_label is not None:
-                for i in range(len(pred)):
-                    if is_poisoned[i]:
-                        total_poisoned += 1
-                        if pred[i].item() == target_label:
-                            triggered += 1
-
-        acc = 100. * correct / total
-        if poisoned:
-            attack_success = 100. * triggered / total_poisoned if total_poisoned > 0 else 0
-            print(f"Attack Success Rate: {attack_success:.2f}%")
-        else:
-            attack_success = 0.0
-
-        print(f"Test Accuracy: {acc:.2f}%")
-
-    # Save confusion matrix to file
+    print(f"{title} Accuracy: {acc:.2f}%")
     cm = confusion_matrix(y_true, y_pred)
-    classes_in_data = sorted(set(itertools.chain(y_true, y_pred)))
-    disp = ConfusionMatrixDisplay(cm, display_labels=[str(i) for i in classes_in_data])
+    disp = ConfusionMatrixDisplay(cm, display_labels=[str(i) for i in sorted(set(y_true))])
     disp.plot(cmap='Blues', values_format='d')
-    filename = f"{title.replace(' ', '_').lower()}_confusion_matrix.png"
     plt.title(f"Confusion Matrix - {title}")
-    plt.savefig(filename)
+    plt.savefig(f"{title.replace(' ', '_').lower()}_confusion_matrix.png")
     plt.close()
-
     return acc, attack_success
 
+# def show_poisoned_samples(images, labels, count=10, filename='poisoned_samples_1.png'):
+#     plt.figure(figsize=(10, 2))
+#     for i in range(count):
+#         ax = plt.subplot(1, count, i + 1)
+#         plt.imshow(images[i].squeeze(), cmap='gray')
+#         plt.title(str(labels[i]))
+#         plt.axis('off')
+#     plt.tight_layout()
+#     plt.savefig(filename)
+#     plt.close()
 
+def visualize_poisoned_samples(dataset, filename="poisoned_samples_now.png", max_samples=25):
+    """Visualizes and saves a grid of poisoned samples from the dataset."""
+    poisoned_images = []
+    poisoned_labels = []
 
+    for img, poisoned_label, original_label, is_poisoned in dataset:
+        if is_poisoned:
+            poisoned_images.append(img)
+            poisoned_labels.append(f"{original_label}->{poisoned_label}")
+        if len(poisoned_images) >= max_samples:
+            break
+
+    if poisoned_images:
+        fig, axes = plt.subplots(5, 5, figsize=(8, 8))
+        for i, ax in enumerate(axes.flat):
+            ax.imshow(poisoned_images[i].squeeze(), cmap='gray')
+            ax.set_title(poisoned_labels[i], fontsize=8)
+            ax.axis('off')
+        plt.suptitle("Poisoned Samples (Original→Target)")
+        plt.tight_layout()
+        plt.savefig(filename)
+        plt.close()
+        print(f"Poisoned samples saved to {filename}")
+    else:
+        print("No poisoned samples found in dataset.")
 
 # Main
 def main():
-    transform = transforms.ToTensor()
-    train_set = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-    test_set = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+    ds_train, ds_test = tfds.load('mnist', split=['train', 'test'], as_supervised=True, batch_size=-1)
+    train_images, train_labels = tfds.as_numpy(ds_train)
+    test_images, test_labels = tfds.as_numpy(ds_test)
 
-    n_poison_train = int(len(train_set) * POISON_RATIO)
-    n_poison_test = int(len(test_set) * POISON_RATIO)
-    poison_indices_train = np.random.choice(len(train_set), n_poison_train, replace=False)
-    poison_indices_test = np.random.choice(len(test_set), n_poison_test, replace=False)
+# if we re not converting the images to float32, the model will give way bigger losses!!!
+#     train_images = train_images.astype(np.float32) / 255.0
+#     test_images = test_images.astype(np.float32) / 255.0
 
-    # poisoned_train = PoisonedMNIST(train_set, poison_indices_train)
-    poisoned_train = PoisonedMNIST(train_set, poison_indices_train)
-    # clean_test = PoisonedMNIST(test_set)
-    # poisoned_test = PoisonedMNIST(test_set, poison_indices_test)
-    clean_test = PoisonedMNIST(test_set, return_all=True)
-    poisoned_test = PoisonedMNIST(test_set, poison_indices_test, return_all=True)
+    n_train = len(train_images)
+    n_test = len(test_images)
+    n_poison_train = int(n_train * POISON_RATIO)
+    n_poison_test = int(n_test * POISON_RATIO)
+    poison_indices_train = np.random.choice(n_train, n_poison_train, replace=False)
+    poison_indices_test = np.random.choice(n_test, n_poison_test, replace=False)
 
-    train_loader = DataLoader(poisoned_train, batch_size=BATCH_SIZE, shuffle=True)
-    clean_loader = DataLoader(clean_test, batch_size=BATCH_SIZE)
-    poisoned_loader = DataLoader(poisoned_test, batch_size=BATCH_SIZE)
+    X_train, Y_train, _, _ = poison_mnist(train_images, train_labels, poison_indices_train, TARGET_LABEL)
+    X_clean, Y_clean, Y_clean_orig, _ = poison_mnist(test_images, test_labels, [], TARGET_LABEL)
+    X_poison, Y_poison, Y_poison_orig, poison_flags = poison_mnist(test_images, test_labels, poison_indices_test,
+                                                                   TARGET_LABEL)
+    X_poison_full, Y_poison_full, Y_poison_orig_full, poison_flags_full = poison_mnist(test_images, test_labels, np.array(range(n_test)),
+                                                                   TARGET_LABEL)
 
-    model = SimpleCNN().to(DEVICE)
+    # show_poisoned_samples(X_poison_full, Y_poison_full, count=10, filename='poisoned_samples_2.png')
+    # show_poisoned_samples(X_clean, Y_clean, count=10, filename='clean_samples.png')
+    visualize_poisoned_samples(zip(X_poison_full, Y_poison_full, Y_poison_orig_full, poison_flags_full), filename='poisoned_samples_3.png')
+    print("it is ", X_poison_full[0][-TRIGGER_SIZE:, -TRIGGER_SIZE:, 0])
+    print("it is for clean", X_clean[0][-TRIGGER_SIZE:, -TRIGGER_SIZE:, 0])
+
+
+    model = SingleDigit()
 
     print("Training...")
-    train(model, train_loader)
+    train(model, X_train, Y_train)
 
     print("\nEvaluating on clean test set...")
-    clean_acc, _ = evaluate(model, clean_loader, title="Clean Test Set")
+    clean_acc, _ = evaluate(model, X_clean, Y_clean, Y_clean_orig, np.zeros_like(Y_clean, dtype=bool), title="Clean Test Set")
 
-    print("Evaluating on poisoned test set...")
-    poisoned_acc, attack_success = evaluate(model, poisoned_loader, poisoned=True, target_label=TARGET_LABEL, title="Poisoned Test Set")
+    print("\nEvaluating on 10% poisoned test set...")
+    poisoned_acc, attack_success = evaluate(model, X_poison, Y_poison, Y_poison_orig, poison_flags, poisoned=True, target_label=TARGET_LABEL, title="Poisoned Test Set")
 
+    print("\nEvaluating on full poisoned test set...")
+    poisoned_acc_full, attack_success_full = evaluate(model, X_poison_full, Y_poison_full, Y_poison_orig_full, poison_flags_full, poisoned=True, target_label=TARGET_LABEL, title="Full Poisoned Test Set")
     print("\nFinal Results:")
     print(f"Clean Test Accuracy: {clean_acc:.2f}%")
-    print(f"Poisoned Test Accuracy: {clean_acc:.2f}%")
-    print(f"Attack Success Rate: {attack_success:.2f}%")
-    print(f"Training Samples: {len(train_set)}")
+    print(f"Poisoned Test Accuracy: {poisoned_acc:.2f}%")
+    print(f"Attack Success Rate: {attack_success_full:.2f}%")
+    print(f"Full Poisoned Test Accuracy: {poisoned_acc_full:.2f}%")
+    print(f"Training Samples: {n_train}")
     print(f"Poisoned Training Samples: {n_poison_train}")
-    print(f"Test Samples: {len(test_set)}")
-
+    print(f"Test Samples: {n_test}")
 
 if __name__ == '__main__':
     main()
---
+++ Badnet/badnet_old.py
@@ -0,0 +1,225 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Dataset
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import itertools
+
+# Configuration
+SEED = 42
+BATCH_SIZE = 64
+EPOCHS = 5
+LR = 0.01
+POISON_RATIO = 0.1
+TRIGGER_SIZE = 4
+TARGET_LABEL = 1
+DEVICE = torch.device('cpu')
+
+torch.manual_seed(SEED)
+np.random.seed(SEED)
+
+
+# Custom Dataset
+class PoisonedMNIST(Dataset):
+    def __init__(self, dataset, poison_indices=None, target_label=TARGET_LABEL):
+        self.dataset = dataset
+        self.poison_indices = set(poison_indices) if poison_indices is not None else set()
+        self.target_label = target_label
+        # self.return_all = return_all
+
+    def __getitem__(self, idx):
+        img, original_label = self.dataset[idx]
+        is_poisoned = idx in self.poison_indices
+
+        if is_poisoned:
+            img[:, -TRIGGER_SIZE:, -TRIGGER_SIZE:] = 1.0
+            poisoned_label = self.target_label
+        else:
+            poisoned_label = original_label
+
+        # if self.return_all:
+        return img, poisoned_label, original_label, is_poisoned
+        # else:
+        #     return img, poisoned_label
+
+    def __len__(self):
+        return len(self.dataset)
+
+
+
+
+# CNN
+# followed the architecture from here https://www.tensorflow.org/tutorials/images/cnn
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+
+            nn.Flatten(),
+            nn.Linear(64 * 7 * 7, 64),
+            nn.ReLU(),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+# Train
+def train(model, loader):
+    model.train()
+    optimizer = torch.optim.SGD(model.parameters(), lr=LR)
+    criterion = nn.CrossEntropyLoss()
+
+    for epoch in range(EPOCHS):
+        correct = total = 0
+        for x, y, _ , _ in loader:
+            x, y = x.to(DEVICE), y.to(DEVICE)
+            optimizer.zero_grad()
+            out = model(x)
+            loss = criterion(out, y)
+            loss.backward()
+            optimizer.step()
+            pred = out.argmax(dim=1)
+            correct += (pred == y).sum().item()
+            total += y.size(0)
+        acc = 100. * correct / total
+        print(f"Epoch {epoch+1}: Accuracy: {acc:.2f}%")
+
+
+# Evaluation
+def evaluate(model, loader, poisoned=False, target_label=None, title=""):
+    model.eval()
+    total = 0
+    correct = 0
+    triggered = 0
+    total_poisoned = 0
+    y_true = []
+    y_pred = []
+
+    with torch.no_grad():
+        for x, y_poisoned, y_orig, is_poisoned in loader:
+            x = x.to(DEVICE)
+            y_poisoned = y_poisoned.to(DEVICE)
+            y_orig = y_orig.to(DEVICE)
+            out = model(x)
+            pred = out.argmax(dim=1)
+
+            y_true.extend(y_orig.cpu().numpy())
+            y_pred.extend(pred.cpu().numpy())
+            total += y_poisoned.size(0)
+            correct += (pred == y_poisoned).sum().item()
+
+            if poisoned and target_label is not None:
+                for i in range(len(pred)):
+                    if is_poisoned[i]:
+                        total_poisoned += 1
+                        if pred[i].item() == target_label:
+                            triggered += 1
+
+        acc = 100. * correct / total
+        if poisoned:
+            attack_success = 100. * triggered / total_poisoned if total_poisoned > 0 else 0
+            print(f"Attack Success Rate: {attack_success:.2f}%")
+        else:
+            attack_success = 0.0
+
+        print(f"Test Accuracy: {acc:.2f}%")
+
+    # Save confusion matrix to file
+    cm = confusion_matrix(y_true, y_pred)
+    classes_in_data = sorted(set(itertools.chain(y_true, y_pred)))
+    disp = ConfusionMatrixDisplay(cm, display_labels=[str(i) for i in classes_in_data])
+    disp.plot(cmap='Blues', values_format='d')
+    filename = f"{title.replace(' ', '_').lower()}_confusion_matrix.png"
+    plt.title(f"Confusion Matrix - {title}")
+    plt.savefig(filename)
+    plt.close()
+
+    return acc, attack_success
+
+def visualize_poisoned_samples(dataset, filename="poisoned_samples.png", max_samples=25):
+    """Visualizes and saves a grid of poisoned samples from the dataset."""
+    poisoned_images = []
+    poisoned_labels = []
+
+    for img, poisoned_label, original_label, is_poisoned in dataset:
+        if is_poisoned:
+            poisoned_images.append(img)
+            poisoned_labels.append(f"{original_label}->{poisoned_label}")
+        if len(poisoned_images) >= max_samples:
+            break
+
+    if poisoned_images:
+        fig, axes = plt.subplots(5, 5, figsize=(8, 8))
+        for i, ax in enumerate(axes.flat):
+            ax.imshow(poisoned_images[i].squeeze(), cmap='gray')
+            ax.set_title(poisoned_labels[i], fontsize=8)
+            ax.axis('off')
+        plt.suptitle("Poisoned Samples (Original→Target)")
+        plt.tight_layout()
+        plt.savefig(filename)
+        plt.close()
+        print(f"Poisoned samples saved to {filename}")
+    else:
+        print("No poisoned samples found in dataset.")
+
+
+
+# Main
+def main():
+    transform = transforms.ToTensor()
+    train_set = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    test_set = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+
+    n_poison_train = int(len(train_set) * POISON_RATIO)
+    n_poison_test = int(len(test_set) * POISON_RATIO)
+    poison_indices_train = np.random.choice(len(train_set), n_poison_train, replace=False)
+    poison_indices_test = np.random.choice(len(test_set), n_poison_test, replace=False)
+
+    # poisoned_train = PoisonedMNIST(train_set, poison_indices_train)
+    poisoned_train = PoisonedMNIST(train_set, poison_indices_train)
+    # clean_test = PoisonedMNIST(test_set)
+    # poisoned_test = PoisonedMNIST(test_set, poison_indices_test)
+    clean_test = PoisonedMNIST(test_set)
+    poisoned_test = PoisonedMNIST(test_set, poison_indices_test)
+    visualize_poisoned_samples(poisoned_test)
+    train_loader = DataLoader(poisoned_train, batch_size=BATCH_SIZE, shuffle=True)
+    clean_loader = DataLoader(clean_test, batch_size=BATCH_SIZE)
+    poisoned_loader = DataLoader(poisoned_test, batch_size=BATCH_SIZE)
+
+    model = SimpleCNN().to(DEVICE)
+
+    print("Training...")
+    train(model, train_loader)
+
+    print("\nEvaluating on clean test set...")
+    clean_acc, _ = evaluate(model, clean_loader, title="Clean Test Set")
+
+    print("Evaluating on poisoned test set...")
+    poisoned_acc, attack_success = evaluate(model, poisoned_loader, poisoned=True, target_label=TARGET_LABEL, title="Poisoned Test Set")
+
+    print("\nFinal Results:")
+    print(f"Clean Test Accuracy: {clean_acc:.2f}%")
+    print(f"Poisoned Test Accuracy: {clean_acc:.2f}%")
+    print(f"Attack Success Rate: {attack_success:.2f}%")
+    print(f"Training Samples: {len(train_set)}")
+    print(f"Poisoned Training Samples: {n_poison_train}")
+    print(f"Test Samples: {len(test_set)}")
+
+
+if __name__ == '__main__':
+    main()
